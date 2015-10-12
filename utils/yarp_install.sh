15734bb1b5ea39d21f36d039db2da2958be1e8f7
Humanoid 2014 workshop <none@none.com>
2015-10-12 12:28:12 +0200

    script updated and tested

--- utils/yarp_install.sh
+++ utils/yarp_install.sh
@@ -1,4 +1,5 @@
 
+
 if [ "$1" != "" ]; then
 export MY_CODE_DIR=$1
     echo "The code will be installed in the directory " $MY_CODE_DIR
@@ -6,6 +7,16 @@ export MY_CODE_DIR=$1
     mkdir $MY_CODE_DIR/bin
     sudo apt-get install libace-dev
     sudo apt-get install cmake
+
+    echo 'source ~/.bash_profile'                                                  >> ~/.bashrc 
+    echo '#YARP'                                                                   >> ~/.bash_profile
+    echo "CODE_DIR="$1                                                            >> ~/.bash_profile
+    echo 'export PATH=$PATH:$CODE_DIR/bin/bin'                                     >> ~/.bash_profile
+    echo 'export YARP_DIR=$CODE_DIR/yarp'                                          >> ~/.bash_profile
+    echo 'export YARP_DATA_DIRS=$CODE_DIR/bin/share/yarp:$CODE_DIR/bin/share/yarp' >> ~/.bash_profile
+    echo 'source $CODE_DIR/yarp/scripts/yarp_completion'                           >> ~/.bash_profile
+    source ~/.bashrc
+
     cd $MY_CODE_DIR
     git clone https://github.com/robotology/yarp.git
     cd yarp
@@ -14,6 +25,8 @@ export MY_CODE_DIR=$1
     cmake .. -DCMAKE_INSTALL_PREFIX:PATH=$MY_CODE_DIR/bin -DINSTALL_WITH_RPATH:STRING=ON -DCREATE_LIB_MATH:STRING=ON
     make
     make install
+    cd ..
+    cd ..
     git clone https://github.com/robotology/icub-main.git ./icub
     cd icub
     mkdir build
@@ -21,7 +34,6 @@ export MY_CODE_DIR=$1
     cmake .. -DCMAKE_INSTALL_PREFIX:PATH=$MY_CODE_DIR/bin -DINSTALL_WITH_RPATH:STRING=ON
     make
     make install
-    echo "In order to use YARP don't forget to add" $MY_CODE_DIR/bin " to the PATH environment variable"
 else
     echo "*****************************************************************"
     echo "******Please provide a path where to install the software!*******"
