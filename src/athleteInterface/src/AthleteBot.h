ebbe722f3a7e08733f926bcaceac1c60bdacb65c
Francesco <francesco.nori@iit.it>
2015-10-12 20:10:47 +0900

    Authors changed

--- src/athleteInterface/src/AthleteBot.h
+++ src/athleteInterface/src/AthleteBot.h
@@ -2,7 +2,7 @@
 
 /*
  * Copyright (C) 2007 RobotCub Consortium
- * Authors: Paul Fitzpatrick
+ * Authors: Paul Fitzpatrick and Francesco Nori
  * CopyPolicy: Released under the terms of the LGPLv2.1 or later, see LGPL.TXT
  *
  */
